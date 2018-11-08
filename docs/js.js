749a08334c977e20955afe5225a3948f3b59a086
anderson420 <44132519+anderson420@users.noreply.github.com>
2018-11-07 20:23:46 -0500

    Update js.js

--- docs/js.js
+++ docs/js.js
@@ -4,19 +4,21 @@ function aleatorio() {
  if(!salto)
      salto=1;
  var maximo = parseInt(document.getElementById("max").value);
- if(minimo > maximo){
+
    minimo= minimo/salto;
    maximo= Math.trunc(maximo/salto);
       var result = (Math.floor(Math.random() * (maximo - minimo + 1) ) + minimo)*salto;
           if(!result){
               document.getElementById("result").innerText = "ingrese valores";
           }else{
+            if(minimo > maximo){
+              
+ 
+               document.getElementById("result").innerText = "el minimo no puede ser mayor que maximo";
+              }
 
               document.getElementById("result").innerText = result;
           }
- }else{
- 
-  document.getElementById("result").innerText = "el minimo no puede se mayor que maximo";
- }
+
     
 }
