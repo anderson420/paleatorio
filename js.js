75d4ab87647daaf055809ef6526cb393f4816b22
anderson420 <44132519+anderson420@users.noreply.github.com>
2018-11-07 19:11:19 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+function aleatorio() {
+ var minimo = parseInt( document.getElementById("min").value);
+ var salto  = parseInt(document.getElementById("salto").value);
+ if(!salto)
+     salto=1;
+ var maximo = parseInt(document.getElementById("max").value);
+  
+ minimo= minimo/salto;
+ maximo= Math.trunc(maximo/salto);
+    var result = (Math.floor(Math.random() * (maximo - minimo + 1) ) + minimo)*salto;
+        if(!result){
+            document.getElementById("result").innerText = "ingrese valores";
+        }else{
+
+            document.getElementById("result").innerText = result;
+        }
+    
+}
